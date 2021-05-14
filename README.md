86692c393ec05cf1e12cf7cdd814c73f6cf56df0
leejss <goldemshine+3@gmail.com>
2021-05-15 00:02:25 +0900

    set typeorm

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 ## Todos
 
-### 1. Set Typescript
+### 1. Set Typescript [Done]
 ### 2. Set TypeORM
+    - createConnection
+### 3. Build Schema
+    - TypeGraphQL
+### 4. Set Server Instacne
+    - apollo-server-express
+    - express
 
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 import { createConnection } from "typeorm";
+import { buildSchema } from "type-graphql";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
 
 const main = async () => {
   // connection
   const { options } = await createConnection();
   console.log(`Connected to ${options.database}`);
+
+  const app = express();
+
+  const schema = await buildSchema({
+    resolvers: [__dirname + "/resolvers/*.js"],
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+  server.applyMiddleware({
+    app,
+  });
+
+  app.listen(4000, () => {
+    console.log("server started on http://localhost:4000/graphql");
+  });
 };
 
 main();
